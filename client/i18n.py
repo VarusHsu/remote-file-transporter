581ea4df1fc3fcaee8be65599661bf3d7a99f58c
Varus <varus570490560@sina.com>
2023-01-14 04:36:06 +0800

    [Feature] Suppose i18n

---
+++ client/i18n.py
@@ -0,0 +1,17 @@
+i18n = {
+    "Remote File Transporter": {
+        "zh_cn": "远程文件传输器",
+        "ja_jp": "リモートファイル転送",
+        "en_us": "Remote File Transporter",
+    },
+    "Setting": {
+        "zh_cn": "设置",
+        "ja_jp": "設定",
+        "en_us": "Setting"
+    },
+    "Welcome": {
+        "zh_cn": "欢迎！按下'设置'按键然后填写你的远程服务器地址。",
+        "ja_jp": "ようこそ！ [設定] ボタンを押して、リモート サーバーのアドレスを入力します。",
+        "en_us": "Welcome, Click 'Setting' then input your remote server address to continue.",
+    }
+}
--- client/main.py
+++ client/main.py
@@ -1,6 +1,8 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QListWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QListWidget, QPushButton
+
+import i18n
 
 config = {
     "mac_os": {
@@ -10,7 +12,11 @@ config = {
         "view_box_move_w": 20,
         "view_box_move_h": 20,
         "view_box_w": 710,
-        "view_box_h": 351,
+        "view_box_h": 360,
+        "setting_button_move_w": 658,
+        "setting_button_move_h": 385,
+        "setting_windows_w": 500,
+        "setting_windows_h": 314,
     },
     "windows": {
         "path_split": "\\"
@@ -21,25 +27,54 @@ config = {
 }
 
 
+class SettingWindows:
+    windows: QWidget
+
+    os: str
+    language: str
+
+    def __init__(self, os: str, language: str):
+        self.os = os
+
+    def render(self):
+        self.windows = QWidget()
+        self.windows.setWindowTitle(i18n.i18n["Setting"][[self.language]])
+        pass
+
+
 class RemoteFileTransporterClient:
     app: QApplication
     windows: QWidget
     view_box: QListWidget
+    setting_button: QPushButton
+    setting_window: SettingWindows
 
     os: str
+    language:str
 
     def __init__(self, os: str):
         self.os = os
+        self.language = "ja_jp"
+
+    def setting_button_on_click(self):
+        self.setting_window = SettingWindows(self.os, self.language)
+        self.setting_window.render()
 
     def render(self):
         self.app = QApplication(sys.argv)
         self.windows = QWidget()
         self.windows.setFixedSize(config[self.os]["windows_w"], config[self.os]["windows_h"])
-        self.windows.setWindowTitle("Remote File Transporter")
+        self.windows.setWindowTitle(i18n.i18n["Remote File Transporter"][self.language])
 
         self.view_box = QListWidget(self.windows)
         self.view_box.move(config[self.os]["view_box_move_w"], config[self.os]["view_box_move_h"])
         self.view_box.resize(config[self.os]["view_box_w"], config[self.os]["view_box_h"])
+        self.view_box.addItem(i18n.i18n["Welcome"][self.language])
+
+        self.setting_button = QPushButton(self.windows)
+        self.setting_button.move(config[self.os]["setting_button_move_w"], config[self.os]["setting_button_move_h"])
+        self.setting_button.setText(i18n.i18n["Setting"][self.language])
+
         self.windows.show()
         sys.exit(self.app.exec())
 
